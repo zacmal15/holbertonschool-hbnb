28212c6c5a528035fe567240d8d8cf720d67bef6
LachyBM <13072@holbertonstudents.com>
2026-06-09 04:03:59 +0200

    did the basic setup

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,3 +2,15 @@
 """
 module for init
 """
+
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+
+    # Placeholder for API namespaces (endpoints will be added later)
+    # Additional namespaces for places, reviews, and amenities will be added later
+
+    return app
