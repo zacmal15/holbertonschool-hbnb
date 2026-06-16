4ee7a798afdacd518646a1f981801d172cd7622b
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-16 23:58:48 +1000

    can now update amenities Still need to add messages

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -47,7 +47,7 @@ class AmenityResource(Resource):
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
         """Update an amenity's information"""
-        amenity = facade.get_amenity(amenity_id)
-        if not amenity:
+        updated_amenity= facade.update_amenity(amenity_id, api.payload)
+        if not updated_amenity:
             return {'error': 'Amenity not found'}, 404
-        return {'id': amenity.id, 'name': amenity.name}, 200
+        return {'id': updated_amenity.id, 'name': updated_amenity.name}, 200
