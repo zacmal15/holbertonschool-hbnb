87c8625d23af1a96bbcde7e779b4e8e5d04910b8
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-16 23:35:30 +1000

    init file changed to add amenity namespaces

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -6,12 +6,14 @@ module for init
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
 
