e6317413dee50939103a5fa8591a67f03a8fb58b
LachyBM <13072@holbertonstudents.com>
2026-06-21 12:24:07 +0200

    fixed lazy not adding imports

--- part2/hbnb/app/api/v1/__init__.py
+++ part2/hbnb/app/api/v1/__init__.py
@@ -3,6 +3,9 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.reviews import api as reviews_ns
 
 def create_app():
     app = Flask(__name__)
@@ -10,4 +13,7 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
     return app
