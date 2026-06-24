46e95da5b8c44ac1f10bb17560b7d960ac9b2472
Zac <13089@holbertonstudents.com>
2026-06-24 08:04:52 +0200

    added config details to init file

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -10,8 +10,11 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+
+    app.config.from_object(config_class)
+    
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     api.add_namespace(users_ns, path='/api/v1/users')
