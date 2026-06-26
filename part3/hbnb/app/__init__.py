2bdddbfa85300612bb31c187be59d3334c4e51ca
Zac <13089@holbertonstudents.com.au>
2026-06-26 09:27:16 +0200

    added auth_ns and namespace

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -5,11 +5,12 @@ module for init
 
 from flask import Flask
 from flask_restx import Api
-from app.extensions import bcrypt
+from app.extensions import bcrypt, jwt
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -17,6 +18,7 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
@@ -24,6 +26,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(auth_ns, path="/api/v1/auth")
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
 
