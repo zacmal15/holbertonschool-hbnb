340ad6cb761dd51ea99a7ef48212a65136012c77
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-30 17:12:13 +1000

    init changes

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,20 +6,18 @@ module for init
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
-from app.extensions import bcrypt, jwt
+from app.extensions import bcrypt, jwt, db
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 
-db = SQLAlchemy()
-
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
-    db.init_app(app)
-
     app.config.from_object(config_class)
+
+    db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
