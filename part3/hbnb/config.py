690968ed4018546dbf5cff3fc1b20700c55f6107
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-30 16:09:04 +1000

    reqs and config changes for implementing sql

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -12,6 +12,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
