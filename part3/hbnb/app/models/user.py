84821f2d8999262f6d92d93b3c89db8c2b2dcd90
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-06 11:27:03 +1000

    hashed out some previous stuff

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -22,36 +22,35 @@ class User(BaseModel):
         """Verify the hashed password."""
         return bcrypt.check_password_hash(self.password, password)
 
-
-    """User class for HBnB users."""
-
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        """Initialise a User instance."""
-        super().__init__()
-
-        if not first_name or len(first_name) > 50:
-            raise ValueError("First name is required and must be 50 characters or less.")
-
-        if not last_name or len(last_name) > 50:
-            raise ValueError("Last name is required and must be 50 characters or less.")
-
-        if not email or not self._valid_email(email):
-            raise ValueError("Valid email is required")
-
-        if not password:
-            raise ValueError("Password is required.")
-
-        if not isinstance(is_admin, bool):
-            raise ValueError("is_admin must be a boolean.")
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.hash_password(password)
-        self.is_admin = is_admin
-        self.places = []
-        self.reviews = []
-
+#
+#    """User class for HBnB users."""
+#
+#    def __init__(self, first_name, last_name, email, password, is_admin=False):
+#        """Initialise a User instance."""
+#        super().__init__()
+#
+ #       if not first_name or len(first_name) > 50:
+#            raise ValueError("First name is required and must be 50 characters or less.")
+#
+#        if not last_name or len(last_name) > 50:
+#            raise ValueError("Last name is required and must be 50 characters or less.")
+#
+#        if not email or not self._valid_email(email):
+#            raise ValueError("Valid email is required")
+#
+#        if not password:
+#            raise ValueError("Password is required.")
+#
+ #       if not isinstance(is_admin, bool):
+#           raise ValueError("is_admin must be a boolean.")
+#        self.first_name = first_name
+#        self.last_name = last_name
+#        self.email = email
+#        self.hash_password(password)
+#        self.is_admin = is_admin
+#        self.places = []
+#        self.reviews = []
+#
     def _valid_email(self, email):
         """Validate email frmat."""
         pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
